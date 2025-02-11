e4b94c01f543cd041093c9d4df120027a4b2e4c3
dalaipooja <147543513+dalaipooja@users.noreply.github.com>
2025-02-11 22:23:16 +0530

    Create node-fs-extra.sh

---
+++ node-fs-extra.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package	: node-fs-extra
+# Version	: latest
+# Source repo	: https://github.com/jprichardson/node-fs-extra
+# Tested on	: UBI:9
+# Language      : Node
+# Travis-Check  : True
+# Script License: Apache License, Version 2 or later
+# Maintainer	: ICH <ich@us.ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+#Install dependencies
+yum install -y nodejs
+
+
+git clone https://github.com/jprichardson/node-fs-extra
+cd  node-fs-extra
+
+if ! npm install fs-extra; then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+if ! npm test; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
